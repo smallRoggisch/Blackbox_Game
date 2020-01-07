61a29028c7e73e3f7e73779cf7e96ba45af11fb7
smallRoggisch <leif.jankowski001@stud.fh-dortmund.de>
2020-01-07 11:57:37 +0100

    Feat: added example Nodes in scenarioCreator and started adding annotations for business objects

--- Game-ejb/src/blackbox/gameplay/treeobjects/Answer.java
+++ Game-ejb/src/blackbox/gameplay/treeobjects/Answer.java
@@ -1,10 +1,24 @@
 package blackbox.gameplay.treeobjects;
 
+import javax.persistence.*;
+
 /**
  *
  */
+@Entity
 public class Answer {
-    long answerID;
-    String answerMessage;
-    Node answerNode;
+    private static long answerIdCounter = 0;
+    @Id
+    private long answerID;
+    @Column
+    private String answerMessage;
+    @Column
+    private Node answerNode;
+
+    public Answer(String answerMessage, Node answerNode)
+    {
+        this.answerID = answerIdCounter++;
+        this.answerMessage = answerMessage;
+        this.answerNode = answerNode;
+    }
 }
